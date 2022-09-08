69519ef5db6042a7e5ca54f2a810783bd83f7522
wangy <376968812@qq.com>
2022-09-08 17:12:43 +0800

    first commit

---
+++ server/wy-blog/src/main/java/com/example/wyblog/BlogApplication.java
@@ -0,0 +1,19 @@
+package com.example.wyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 博客应用程序
+ *
+ * @author zfkkyjy
+ * @date 2022/09/08
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
